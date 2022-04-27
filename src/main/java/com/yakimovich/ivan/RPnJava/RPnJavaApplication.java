362bd5fa23d5df11c3cbbb2b884cf0c4dbbf0ae8
Yadchenko, Anton <a.yadchenko@itransition.com>
2022-04-27 20:41:40 +0300

    first commit

---
+++ src/main/java/com/yakimovich/ivan/RPnJava/RPnJavaApplication.java
@@ -0,0 +1,13 @@
+package com.yakimovich.ivan.RPnJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RPnJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RPnJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yakimovich/ivan/RPnJava/RPnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakimovich.ivan.RPnJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RPnJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
